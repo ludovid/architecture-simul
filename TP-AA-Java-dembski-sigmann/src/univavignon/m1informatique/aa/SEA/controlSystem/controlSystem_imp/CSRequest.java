d9756d95e83bef5e53da9e7dda390f3ae75f2ea9
Utilisateur <Utilisateur@10.122.24.214>
2015-11-16 10:25:59 +0100

    CSUpdate

---
+++ TP-AA-Java-dembski-sigmann/src/univavignon/m1informatique/aa/SEA/controlSystem/controlSystem_imp/CSRequest.java
@@ -0,0 +1,5 @@
+package univavignon.m1informatique.aa.SEA.controlSystem.controlSystem_imp;
+
+public class CSRequest { 
+
+}
---
+++ TP-AA-Java-dembski-sigmann/src/univavignon/m1informatique/aa/SEA/controlSystem/controlSystem_imp/Optimize.java
@@ -0,0 +1,10 @@
+package univavignon.m1informatique.aa.SEA.controlSystem.controlSystem_imp;
+
+public class Optimize { 
+	public Optimize() {
+		
+	}
+	public void algo() {
+		System.out.println("Ca marche");
+	}
+}

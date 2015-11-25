8d80f79a90b98855bfc24f4803e9dcade6fb5ebb
adriansalas <adriansalas@10.122.41.211>
2015-11-25 15:48:02 +0100

    -changé elevatorFactory

--- TP-AA-Java-dembski-sigmann/src/univavignon/m1informatique/aa/SEA/elevator/api/ElevatorCommandFactory.java
+++ TP-AA-Java-dembski-sigmann/src/univavignon/m1informatique/aa/SEA/elevator/api/ElevatorCommandFactory.java
@@ -1,7 +1,4 @@
 package univavignon.m1informatique.aa.SEA.elevator.api;
-import java.io.File;
-
-import univavignon.m1informatique.aa.SEA.elevator.impl.ElevatorShaft;
 
 public class ElevatorCommandFactory {
 
@@ -9,12 +6,8 @@ public class ElevatorCommandFactory {
 	 * 
 	 * @return 
 	 */
-	public static IElevatorCommand getElevatorCommand(File setting) { 
-	
-		// mettre les param du fichier pour construire les objets de shaft
-		ElevatorShaft ES = new ElevatorShaft(2, 5, 3, 10, 1); 
-		
-		
+	public static IElevatorCommand getElevatorCommand() { 
+		// TODO Auto-generated method
 		return null;
 	 } 
 
